3797f746ed5a4c3ee681a40b6c2ab7d95b62a575
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-02-05 15:40:06 +0100

    Updated some project stuff (icon font, vuetify, ...)

--- packages/client/src/main.ts
+++ packages/client/src/main.ts
@@ -3,10 +3,19 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Plugins
+import Vuetify from 'vuetify';
+
+// Stylesheets
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.min.css';
+
 Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: (h) => h(App)
 }).$mount('#app');
