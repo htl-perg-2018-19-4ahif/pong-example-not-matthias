83f7b25c4cf349c9d8f2ec289238680a4a9f8171
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-02-05 20:31:04 +0100

    Added home design

--- packages/client/src/router.ts
+++ packages/client/src/router.ts
@@ -8,9 +8,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'login',
       component: () => import('@/views/Login.vue')
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/Home.vue')
     }
   ]
 });
---
+++ packages/client/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-content class="pt-5">
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6 lg4>
+          <!-- Card -->
+          <v-card class="elevation-12">
+            <v-toolbar dark>
+              <v-toolbar-title>Play</v-toolbar-title>
+            </v-toolbar>
+
+            <v-card-text>
+              <v-btn outline block raised @click="onJoinQueueClicked()">Join Queue</v-btn>
+
+              <v-divider class="mt-4"/>
+            </v-card-text>
+
+            <v-card-actions class="px-3 pb-3">
+              <v-btn small outline block @click="onCreateLobbyClicked()">Create Lobby</v-btn>
+              <v-btn small outline block @click="onJoinLobbyClicked()">Join Lobby</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Home extends Vue {
+  private onJoinQueueClicked() {
+    // TODO: implement logic
+  }
+
+  private onCreateLobbyClicked() {
+    // TODO: implement logic
+  }
+
+  private onJoinLobbyClicked() {
+    // TODO: implement logic
+  }
+}
+</script>
+
