aad20e171d9376b0df786ffbd088008b604b50d0
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-02-06 10:24:12 +0100

    Changed order

--- packages/client/src/router.ts
+++ packages/client/src/router.ts
@@ -8,14 +8,14 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/Login.vue')
-    },
-    {
       path: '/',
       name: 'home',
       component: () => import('@/views/Home.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue')
     },
     {
       path: '/new_lobby',
