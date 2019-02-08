d2d9bde1fdb3b1175c29332f35fcaef448ed4131
not-matthias <26800596+not-matthias@users.noreply.github.com>
2019-02-08 17:36:27 +0100

    Added collision (not finished yet)

--- packages/client/src/game/ball.ts
+++ packages/client/src/game/ball.ts
@@ -11,39 +11,77 @@ export class Ball {
   ) {}
 
   public update(player1: Player, player2: Player) {
-    this.move(this.velocity.x, this.velocity.y);
-  }
-
-  private move(x: number, y: number) {
     // Check for collision with:
     // - Player 1 (Pad)
     // - Player 2 (Pad)
     // - Border (top, bottom)
 
+    //
+    // Check player collision
+    //
+
+    if (this.isColliding(player1)) {
+      console.log('player1 collision detected.');
+    } else if (this.isColliding(player2)) {
+      console.log('player2 collision detected.');
+    }
+
+    //
     // Check border collision
+    //
     const topBorder = -this.canvas.y / 2 + this.cirlce.radius / 2;
     const bottomBorder = this.canvas.y / 2 - this.cirlce.radius / 2;
     const leftBorder = -this.canvas.x / 2 + this.cirlce.radius / 2;
     const rightBorder = this.canvas.y / 2 - this.cirlce.radius / 2;
 
     // Top border
-    if (this.graphics.y + y < topBorder) {
-      // TODO: implement
+    if (this.graphics.y + this.velocity.y < topBorder) {
+      this.velocity.y *= -1;
     }
     // Bottom border
-    else if (this.graphics.y + y > bottomBorder) {
-      // TODO: implement
+    else if (this.graphics.y + this.velocity.y > bottomBorder) {
+      this.velocity.y *= -1;
     }
     // Left border
-    else if (this.graphics.x + x < leftBorder) {
-      // TODO: implement
+    else if (this.graphics.x + this.velocity.x < leftBorder) {
+      this.velocity.x *= -1;
     }
     // Right border
-    else if (this.graphics.x + x > topBorder) {
-      // TODO: implement
-    } else {
-      this.graphics.x += x;
-      this.graphics.y += y;
+    else if (this.graphics.x + this.velocity.x > rightBorder) {
+      this.velocity.x *= -1;
     }
+
+    //
+    // Move the ball
+    //
+    this.move(this.velocity.x, this.velocity.y);
+  }
+
+  private move(x: number, y: number) {
+    this.graphics.x += x;
+    this.graphics.y += y;
+  }
+
+  private isColliding(player: Player): boolean {
+    // if(pointX > rectX && pointX < rectX + rectWidth && pointY > rectY && pointY < rectY + rectHeight){
+    if (
+      this.graphics.x > player.graphics.x &&
+      this.graphics.x < player.graphics.x + player.graphics.width &&
+      this.graphics.y > player.graphics.y &&
+      this.graphics.y < player.graphics.y + player.graphics.height
+    ) {
+      //the point is inside the rectangle
+    }
+
+    if (
+      player.graphics.x < this.cirlce.x + this.cirlce.radius &&
+      player.graphics.x + player.graphics.width > this.cirlce.x &&
+      player.graphics.y < this.cirlce.y + this.cirlce.radius &&
+      player.graphics.y + player.graphics.height > this.cirlce.y
+    ) {
+      return true;
+    }
+
+    return false;
   }
 }
